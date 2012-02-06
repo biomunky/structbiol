da77a3bbd3ffc078adcd9e7c1763ee4e38b06590
biomunky <biomunky@gmail.com>
2012-02-06 12:45:36 +0000

    Updated README. Changed format of core.clj

--- src/structbiol/core.clj
+++ src/structbiol/core.clj
@@ -1,18 +1,16 @@
 (ns structbiol.core
   (:gen-class)
   (:use [structbiol.dssp]
-		[structbiol.pdb]
-		[clojure.contrib.math :only [expt sqrt]])
-	(:import (java.io File)))
+	[structbiol.pdb]
+	[clojure.contrib.math :only [expt sqrt]])
+  (:import (java.io File)))
 
 (defn -main [& args]
   (def dssp-dir (File. "/Users/biomunky/Dropbox/postdoc/data-analysis/dssp"))
-	
-	(doall (for [f (.listFiles dssp-dir)]
-		(let [d-file (read-dssp (str (.getAbsoluteFile f)))
-					cs     (chains d-file)]
-		(if (= 1 (count cs))
-			(do (println "Chunking" (.getName f))
-					(write-chunks-to-file (first cs) (remove-bad-residues d-file) (.getName f)))))))
-					
+  (doall (for [f (.listFiles dssp-dir)]
+	   (let [d-file (read-dssp (str (.getAbsoluteFile f)))
+		 cs     (chains d-file)]
+	     (if (= 1 (count cs))
+	       (do (println "Chunking" (.getName f))
+		   (write-chunks-to-file (first cs) (remove-bad-residues d-file) (.getName f)))))))
   (flush))
